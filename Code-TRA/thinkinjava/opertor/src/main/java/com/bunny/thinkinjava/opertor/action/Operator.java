4cf8eeec62707dc743450863a040f119f05427e0
chaizhen <chaizhen03@meituan.com>
2017-08-23 00:45:40 +0800

    opertor_code

---
+++ Code-TRA/thinkinjava/opertor/src/main/java/com/bunny/thinkinjava/opertor/action/Operator.java
@@ -0,0 +1,43 @@
+package com.bunny.thinkinjava.opertor.action;
+
+/**
+ * \* Created By Zhen Chai on 2017/8/23.
+ * \* Description:操作符相关操作
+ * \* @author zhenchai
+ * \* @author zhenchai@foxmail.com
+ * \* @version 1.0.0
+ * \
+ */
+public class Operator {
+
+    public static void main(String[] args) {
+        /**
+         * new Random时的参数seed：
+         * 设置固定的值，将保证多次运行该程序时生成的随机值的序列一致
+         */
+        /*
+        Random random = new Random(100);
+        int a = random.nextInt(100) + 1;
+        int b = random.nextInt(100) + 1;
+        System.out.println(a + " "
+                + b);
+        */
+
+
+        /**
+         * 不可将一个非布尔值当作布尔值在逻辑表达式中使用
+         */
+        /*
+        Random random = new Random(100);
+        int a = random.nextInt(100) + 1;
+        int b = random.nextInt(100) + 1;
+        System.out.println("a && b is " + (a && b));
+        */
+
+
+
+
+
+    }
+
+}
